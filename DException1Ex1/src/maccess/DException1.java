8e2ec7c1b163efeab690585e83f4dc72924c0ed4
Jagannath <Jagannath@DESKTOP-6V9A5KF>
2019-03-31 19:16:56 +0530

    FirstCommit All the Files

---
+++ DException1Ex1/src/maccess/DException1.java
@@ -0,0 +1,30 @@
+package maccess;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class DException1 {
+
+	public static void main(String[] args) {
+
+		xyz: while (true) {
+			try {
+				Scanner s = new Scanner(System.in);
+				System.out.println("Enter the val:");
+				int val = s.nextInt();
+
+				int z = 44 / val;//exception will be raised
+				
+				System.out.println("The Val of z:" + z);
+				s.close();
+				break xyz;
+			} catch (ArithmeticException ae) {
+				System.out.println("only non zero value..");
+				System.out.println("details: " + ae.getMessage());
+			} catch (InputMismatchException ime) {
+				System.out.println("Only integer value..");
+				System.out.println("Details: " + ime.getMessage());
+			}
+		} // while close
+	}
+}
